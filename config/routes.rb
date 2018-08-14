ce50cea2217c8b46d7ee352f94a8f99db000d8c2
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-08-14 17:03:25 +0200

    finished the To-Do List

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "tasks#index"
-  get 'tasks/:id', to: "tasks#show", as: :task
+  root "tasks#index"  #GET /tasks
+  
+  get 'new', to: "tasks#new", as: :new  #GET new
+  post'tasks', to: "tasks#create" # POST /tasks
+  
+  get 'tasks/:id', to: "tasks#show", as: :task  #GET tasks/:id
+ 
+  get 'tasks/:id/edit', to: "tasks#edit" , as: :edit #GET tasks/:id/edit 
+  patch 'tasks/:id', to: "tasks#update"   #PATCH tasks/:id
+  
+  delete 'tasks/:id', to: "tasks#destroy",  as: :destroy #DELETE tasks/:id
 
 end

2f39f3765448f432a6e067fef975655c1c734c7d
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-08-14 14:15:40 +0200

    task list index and show completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,26 @@
 class TasksController < ApplicationController   
   def index
+    @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new 
+  end
+
+  def create
+  end
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+
+  
 end
